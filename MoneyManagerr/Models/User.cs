04750e96db02029ae054263d8d8a70aed015dc6f
Patipon <57050269@kmitl.ac.th>
2019-01-13 12:34:23 +0700

    Update : add Forms and Models

---
+++ MoneyManagerr/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagerr.Models
+{
+    class User
+    {
+        private string name;
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }         
+        }
+    }
+}
--- MoneyManagerr/Program.cs
+++ MoneyManagerr/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoneyManagerr.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace MoneyManagerr
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
